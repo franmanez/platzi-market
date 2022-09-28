145635840da7de5c2c5a1368ded44e81b5928b74
Fran <francisco.manez@upc.edu>
2022-09-28 14:21:17 +0200

    jwt and others

--- src/main/java/com/platzi/market/web/config/SwaggerConfig.java
+++ src/main/java/com/platzi/market/web/config/SwaggerConfig.java
@@ -15,6 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
+                //.apis(RequestHandlerSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.platzi.market.web.controller"))
                 .build();
     }
--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.platzi.market.web.controller;
 
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
+    @ApiOperation("obtiene todos los productos")
+    @ApiResponse(code = 200, message = "OK")
     public List<Product> getAll() {
         return productService.getAll();
     }
