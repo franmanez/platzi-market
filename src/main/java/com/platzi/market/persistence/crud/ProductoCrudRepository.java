23980fb461c6f21b904dc154aa83198a5f00b3f8
Fran <francisco.manez@upc.edu>
2022-09-29 08:41:42 +0200

    end course commit

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -3,7 +3,6 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-import java.awt.color.ProfileDataException;
 import java.util.List;
 import java.util.Optional;
 
